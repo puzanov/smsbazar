8bf37a59ad32dff6fcc5ad30dbe4d48dc691c335
puzanov <puzanov@gmail.com>
2011-01-06 17:50:27 +0600

    Первый скелет Рельсов

---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = 'bb4b83771a2c930583bc5c42ff4c7921b33236b36e2ad82f33497803001b90f302fcea43b9b6d522387a8a491174a9f9c0121a5a023acb4f7c4c377e2e0d7e9f'
