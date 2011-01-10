a95b0f2b1903c3fd46b8986e1fd997f4cd863fff
puzanov <puzanov@gmail.com>
2011-01-10 19:26:43 +0600

    удалил ненужные диры тест для монги уже не нужен logica smpp simalator заменил на другой

--- mongotest/test.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'mongo'
-
-connection = Mongo::Connection.new # (optional host/port args)
-connection.database_names.each { |name| puts name }
-connection.database_info.each { |info| puts info.inspect}
--- smpptest/simulator/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp smpp.jar:smscsim.jar com.logica.smscsim.Simulator
