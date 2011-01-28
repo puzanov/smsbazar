e97edf492a20534dfb8b193ae40c664b55d5886b
puzanov <puzanov@gmail.com>
2011-01-28 11:03:24 +0600

    изменил модель и играю с шаблонами

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -1,30 +1,6 @@
 class HomeController < ApplicationController
   def index
-    begin
-      puts "Starting SMS Gateway. Please check the log at #{LOGFILE}"
-
-      # SMPP properties. These parameters work well with the Logica SMPP simulator.
-      # Consult the SMPP spec or your mobile operator for the correct settings of
-      # the other properties.
-      config = {
-        :host => '127.0.0.1',
-        :port => 11111,
-        :system_id => 'test',
-        :password => 'test',
-        :system_type => '', # default given according to SMPP 3.4 Spec
-        :interface_version => 52,
-        :source_ton  => 0,
-        :source_npi => 1,
-        :destination_ton => 1,
-        :destination_npi => 1,
-        :source_address_range => '',
-        :destination_address_range => '',
-        :enquire_link_delay_secs => 10
-      }
-      gw = SampleGateway.new
-      gw.start(config)
-    rescue Exception => ex
-      puts "Exception in SMS Gateway: #{ex} at #{ex.backtrace.join("\n")}"
-    end  
+    @f = 1
+    @p = params[:status]
   end
 end
--- web/app/models/adv.rb
+++ web/app/models/adv.rb
@@ -3,4 +3,6 @@ class Adv
   field :phone, :type => String
   field :content, :type => String
   field :node_id, :type => String
+  field :city, :type => String
+  field :price, :type => Integer
 end
