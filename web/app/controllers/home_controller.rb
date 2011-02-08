e10f151a802524fb1d1cc4588a6e75663f572e4e
Oleg Puzanov <puzanov@gmail.com>
2011-02-08 23:03:20 +0600

    браузим по категориям

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -4,8 +4,13 @@ class HomeController < ApplicationController
   attr_accessor :menu_manager
   
   def index
+    id = params[:id]
     @menu_manager = MenuManager.new
-    root_id = Tree.root.id.to_s      
-    @cats = @menu_manager.get_particular_menu root_id
+    if id.present?
+      node_id = id
+    else
+      node_id = Tree.root.id.to_s      
+    end
+    @cats = @menu_manager.get_particular_menu node_id
   end
 end
