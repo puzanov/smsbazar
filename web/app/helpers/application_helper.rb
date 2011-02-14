86907408b92fc618d4cd79ece023898724e9f925
Oleg Puzanov <puzanov@gmail.com>
2011-02-14 19:37:48 +0600

    форматируем телефон

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
   def format_phone_number phone_number
+    regexp = /^996(\d{3})(\d{6})/is
+    matches = regexp.match phone_number
+    if matches
+      return "0-#{matches[1]}-#{matches[2]}"
+    end
   end
 end
