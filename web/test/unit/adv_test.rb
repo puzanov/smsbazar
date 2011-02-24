901ebff1c8ea7edbeef8b0ce2cde8275890a5430
Oleg Puzanov <puzanov@gmail.com>
2011-02-24 09:41:31 +0600

    убрал ненужный тест

--- web/test/unit/adv_test.rb
+++ web/test/unit/adv_test.rb
@@ -14,22 +14,7 @@ class AdvTest < Test::Unit::TestCase
     adv.price = 10000
     adv.ctime = Time.new.to_i
     adv.save
-    adv.destroy
-  end
-
-  def test_migrate
-    advs = Adv.find :all
-    advs.each do |adv|
-      new_adv = Adv.new
-      new_adv.phone = adv.phone
-      new_adv.content = adv.content
-      new_adv.node_id = adv.node_id
-      new_adv.city = adv.city
-      new_adv.price = adv.price
-      new_adv.ctime = adv.ctime
-      adv.destroy
-      new_adv.save
-    end
+    #adv.destroy
   end
 end
 
